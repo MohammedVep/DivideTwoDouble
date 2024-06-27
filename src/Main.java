bca3bf496b709159fd5d6cd1b20ff7c99ab5c3a5
MohammedVep <98326062+MohammedVep@users.noreply.github.com>
2024-06-27 16:16:39 -0400

    Completed Divide Two Double Variables exercise.

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        double x = 34.33;
+        double y = 22.43;
+        double result = x / y;
+        System.out.println("Solution is " + result);
+    }
+}
